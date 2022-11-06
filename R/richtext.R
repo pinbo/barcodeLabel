024c0d7d49b103d9e65ba5e6e2ff9c4f1c6668ac
Junli Zhang <zhjl86@gmail.com>
2022-11-06 08:57:26 -0800

    format richtext

--- R/richtext.R
+++ R/richtext.R
@@ -1,5 +1,32 @@
 
 ####
+#' Draw formatted text labels
+#' richtext() can draw text labels with mixed fontface from markdown syntax (only support * quotes)" \*italic\*, or \*\*bold\*\*, or \*\*\*bold italic\*\*\*
+#' @param txt a string
+#' @param x coordinate on the x axis
+#' @param y coordinate on the y axis
+#' @param gp An object of class "gpar", typically the output from a call to the function gpar.
+#' @param unit A string indicating the default units to use if x or y are only given as numeric values.
+#' @param hjust A number specifying horizontal justification.
+#' @param vjust A number specifying vertical justification. 
+#' @param draw A logical value indicating whether graphics output should be produced.
+#' @param useMarkdown whether treat ** quotes as markdown (only support fontfaces)
+#'
+#' @return richtext() returns a gList that can be used in grid::grid.draw
+#' @export
+#'
+#' @examples
+#' library(grid)
+#' grid.newpage()
+#' vp = viewport(x=0.5, y=0.5, width=unit(3.5, "inches"), height=unit(1, "inches"), gp=gpar(fontsize=12, lineheight=0.8))
+#' pushViewport(vp)
+#' grid.rect()
+#' # can use "\n" for a new line
+#' txt = "plain text and\n *italic text* and **bold text** and\n***bold italic text***"
+#' gg = richtext(txt, draw = TRUE)
+#' # grid.draw(gg)
+#' popViewport(1)
+#' 
 richtext = function(txt, x=0.5, y=0.5, gp = grid::gpar(), unit="npc", hjust=0.5, vjust=0.5, draw=TRUE, useMarkdown=TRUE){#x, y is for top left by default
   if (!useMarkdown | !grepl("\\*", txt)){
     return(grid::grid.text(txt, x=x, y=y, gp=gp, default.units = unit, hjust=hjust, vjust=vjust, draw = draw))
@@ -35,15 +62,17 @@ richtext = function(txt, x=0.5, y=0.5, gp = grid::gpar(), unit="npc", hjust=0.5,
   }
 }
 
-
-seprichlines = function(txt){ # separate lines and make up split * quotes
+## separate lines and make up split * quotes in case "\n" is inside a * quote
+seprichlines = function(txt){ 
   txtpieces = strsplit(txt, "\n")[[1]]
   if (length(txtpieces)>1){ # if there is newline
     nn = gregexpr('\n', txt)[[1]] # newline positions
     bb = gregexpr('\\*+', txt)[[1]] # individual star quotes
     bblen = attributes(bb)$match.length
     t1 = sapply(nn, function(x){
-      min(which(bb>x))
+      mm = which(bb>x)
+      if (length(mm) > 0) min(mm)
+      else 1 # only even number are inside *quotes, so give 1 means not inside
     })
     newlinepos = which(t1%%2==0)
     if (length(newlinepos)>0){# if any newlines are inside the * quotes
@@ -63,7 +92,8 @@ seprichlines = function(txt){ # separate lines and make up split * quotes
   txtpieces
 }
 
-#get font face: parse markdown 
+## split a rich line by fontfaces
+# return a list for split text pieces and corresponding fontfaces
 getfontface = function(txt){
   # txt = "a *abc* ef **d*** e"
   nc = nchar(txt)
@@ -86,50 +116,8 @@ getfontface = function(txt){
   }
 }
 
-# # old but working version
-# # get font face: parse markdown 
-# getfontface = function(txt){
-#   # txt = "and **bold** again *."
-#   nc = nchar(txt)
-#   txtpieces = strsplit(txt, "\\*+", perl=T)[[1]]
-#   if(txtpieces[1]=="") txtpieces = txtpieces[-1]
-#   fontc = c()
-#   textsegnum = 1
-#   aa = gregexpr('\\*+(.+?)\\*+', txt)[[1]] # paired star quotes
-#   if(aa[1]!=1) {
-#     fontc = 1
-#     textsegnum = 2
-#   }
-#   if (aa[1] > 0){ # if there are * quotes
-#     bb = gregexpr('\\*+', txt)[[1]] # single star quotes
-#     bblen = attributes(bb)$match.length
-#     # if odd number of \*+, split by pair
-#     if (length(bblen)%%2 == 1)
-#       txtpieces = strsplit(txt, "(\\*+)(?=.+?\\*+)", perl=T)[[1]]
-#     if(txtpieces[1]=="") txtpieces = txtpieces[-1]
-#     star2font = c(0,2,1,3) # number of * for normal, bold, italic, and bold italic
-#     naa = length(aa)
-#     for (i in 1:naa){
-#       x = aa[i]
-#       pos = which(bb==x)
-#       len1 = bblen[pos] # left star quote length
-#       len2 = bblen[pos+1] # right star quote length
-#       cc = min(bblen[pos:(pos+1)]) # star length
-#       dd = which(star2font == cc)
-#       fontc = c(fontc, dd)
-#       if (bb[pos+1]+len2-1<nc) fontc = c(fontc, 1)
-#       if (len1 > len2) {# add star to left
-#         txtpieces[textsegnum] = paste0(rep("*", len1-len2), txtpieces[textsegnum])
-#       } else if (len1 < len2){
-#         txtpieces[textsegnum] = paste0(txtpieces[textsegnum], rep("*", len2-len1))
-#       }
-#       textsegnum = textsegnum + 2
-#     }
-#   } else txtpieces = strsplit(txt, "(\\*+)(?=.+?\\*+)", perl=T)[[1]]
-#   return(list(txt=txtpieces, font=fontc))
-# }
 
-# get grob of each text: just one line: no newline, so split by newline first
+## get grob of each text: just one line: no newline, so split by newline first
 mixfontGrob = function(txtvec, fontvec, x, y, gp=gpar(), unit="pt", hjust = 0){#vector of text and corresponding font faces
   txtpieces = txtvec
   n = length(txtpieces)
@@ -157,12 +145,12 @@ mixfontGrob = function(txtvec, fontvec, x, y, gp=gpar(), unit="pt", hjust = 0){#
   return(tmplist)
 }
 
-## split text by "/n"
+## get x and y cursur positions for multi-line text (has \n)
 getxy = function(text, gp = grid::gpar(), unit="pt"){
   tt1 = strsplit(text, "\n")[[1]]
   tt2 = tt1[length(tt1)] # the last line
-  g1 = grid::textGrob(tt2, gp=gp, hjust=0, vjust=1, name="xxxg1") # for x cursor
-  g2 = grid::textGrob(text, gp=gp, hjust=0, vjust=1, name="yyyg2")  # for height y
+  g1 = grid::textGrob(tt2, gp=gp, hjust=0, vjust=1) # for x cursor
+  g2 = grid::textGrob(text, gp=gp, hjust=0, vjust=1)  # for height y
   w1 = grid::convertWidth(grid::grobWidth(g1), unit)
   h1 = grid::convertHeight(grid::grobHeight(g1), unit)
   h2 = grid::convertHeight(grid::grobHeight(g2), unit)
@@ -171,7 +159,7 @@ getxy = function(text, gp = grid::gpar(), unit="pt"){
   return(list(x=w1, y=h2-h1,multline=multline))
 }
 
-# return total width and height of text
+## return total width and height of text
 getxy2 = function(text, gp = grid::gpar(), unit="pt"){
   g2 = grid::textGrob(text, gp=gp, hjust=0, vjust=1)  # for height y
   w2 = grid::convertWidth(grid::grobWidth(g2), unit)
